36c75d6065bbf98bfc2fabaca8bc91a598fbe41f
Abo3Hamza <a6672339@gmail.com>
2026-04-21 14:02:14 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,18 @@ const urlsToCache = [
   '',
   'index.html',
   'manifest.json',
-  // Add other assets to cache here, e.g., CSS, JS, images
+  'icons/icon-48x48.png',
+  'icons/icon-72x72.png',
+  'icons/icon-96x96.png',
+  'icons/icon-128x128.png',
+  'icons/icon-144x144.png',
+  'icons/icon-152x152.png',
+  'icons/icon-192x192.png',
+  'icons/icon-384x384.png',
+  'icons/icon-512x512.png',
+  'script.js',
+  'style.css',
+
 ];
 
 self.addEventListener('install', event => {
